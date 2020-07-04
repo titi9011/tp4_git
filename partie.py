6bcfa92f56bcb034aa77cb5d19599c4cc0dbfc00
titi9011 <thierry.blais.1@ulaval.ca>
2020-07-04 16:04:18 -0400

    commit

--- partie.py
+++ partie.py
@@ -307,7 +307,7 @@ if __name__ == "__main__":
     # Point d'entrée du programme. On initialise une nouvelle partie, et on appelle la méthode jouer().
     partie = Partie()
 
-    gagnant = partie.jouer
+    gagnant = partie.jouer()
 
     print("------------------------------------------------------")
     print("Partie terminée! Le joueur gagnant est le joueur", gagnant)
