6046e0dacbc22eebafeb6b390092ec7245838d4f
titi9011 <thierry.blais.1@ulaval.ca>
2020-07-22 10:40:40 -0400

    hehehehehe

--- engine.py
+++ engine.py
@@ -8,19 +8,47 @@ from time import sleep
 
 class Engine():
 
-    #initialisation de l'arbre des possibilités
-    def __init__(self):
-        pass
-
     def print_damier(self, dic):
+        """Affiche un damier.
+
+        Args:
+            dic (dict): Dictionnaire d'une position.
+
+        Returns:
+            (str): Représentation des pieces du damier.
+        """
         return Damier().print_damier(dic)
     
-    def print_dic_list(self, dic):
-        for dictionnaire in dic:
+    def print_dic_list(self, list_dic):
+        """Afficher une list de damier.
+
+        Args:
+            list_dic (list): Liste de plusieurs dictionnaires.
+
+        Returns:
+            (str): Représentation des damiers.
+        """
+        for dictionnaire in list_dic:
             self.print_damier(dictionnaire)
 
 
     def piece_peut_se_deplacer_vers_modif(self, position_piece, position_cible, dic):
+        """Cette méthode détermine si une pièce (à la position reçue) peut se déplacer à une certaine position cible.
+        On parle ici d'un déplacement standard (et non une prise).
+        Une pièce doit être positionnée à la position_piece reçue en argument (retourner False autrement).
+        Une pièce de type pion ne peut qu'avancer en diagonale (vers le haut pour une pièce blanche, vers le bas pour
+        une pièce noire). Une pièce de type dame peut avancer sur n'importe quelle diagonale, peu importe sa couleur.
+        Une pièce ne peut pas se déplacer sur une case déjà occupée par une autre pièce. Une pièce ne peut pas se
+        déplacer à l'extérieur du damier.
+
+        Args:
+            position_piece (Position): La position de la pièce source du déplacement.
+            position_cible (Position): La position cible du déplacement.
+            dic (dict): Dictionnaire des positions.
+
+        Returns:
+            bool: True si la pièce peut se déplacer à la position cible, False autrement.
+        """
         #si la position est dans le damier et s'il y a une piece sur la case et si la position_cible n'est pas occupée
         if Damier().position_est_dans_damier(position_cible) and position_piece in dic and not position_cible in dic:
 
@@ -28,9 +56,7 @@ class Engine():
                 if position_cible in position_piece.quatre_positions_diagonales():
                     return True
                 else:
-
                     return False
-
             elif dic[position_piece].est_noire():
                 if position_cible in position_piece.positions_diagonales_bas():
                     return True
@@ -45,7 +71,21 @@ class Engine():
             return False
 
     def piece_peut_sauter_vers_modif(self, position_piece, position_cible, dic):
-        #TODO: À tester - compléter
+        """Cette méthode détermine si une pièce (à la position reçue) peut sauter vers une certaine position cible.
+        On parle ici d'un déplacement qui "mange" une pièce adverse.
+        Une pièce doit être positionnée à la position_piece reçue en argument (retourner False autrement).
+        Une pièce ne peut que sauter de deux cases en diagonale. N'importe quel type de pièce (pion ou dame) peut sauter
+        vers l'avant ou vers l'arrière. Une pièce ne peut pas sauter vers une case qui est déjà occupée par une autre
+        pièce. Une pièce ne peut faire un saut que si elle saute par dessus une pièce de couleur adverse.
+
+        Args:
+            position_piece (Position): La position de la pièce source du saut.
+            position_cible (Position): La position cible du saut.
+            dic (dict): Dictionnaire des positions.
+
+        Returns:
+            bool: True si la pièce peut sauter vers la position cible, False autrement.
+        """
         position_piece_mange = position_piece.position_mange(position_cible)
         #s'il y a une piece à manger
         if position_piece_mange in dic:
@@ -61,6 +101,14 @@ class Engine():
                 return False
 
     def dic_blanc(self, dic):
+        """Permet de filtrer seulement les pièces blanches d'un dictionnaire.
+
+        Args:
+            dic (dict): Dictionnaire des positions.
+
+        Returns:
+            (dict): Dictionnaire des positions sans les pièces noires.
+        """
         dic_blanc = {}
         for position, piece in dic.items():
             if piece.couleur == 'blanc':
@@ -68,6 +116,14 @@ class Engine():
         return dic_blanc
 
     def dic_noir(self, dic):
+        """Permet de filtrer seulement les pièces noires d'un dictionnaire.
+
+        Args:
+            dic (dict): Dictionnaire des positions.
+
+        Returns:
+            (dict): Dictionnaire des positions sans les pièces blanches.
+        """
         dic_noir = {}
         for position, piece in dic.items():
             if piece.couleur == 'noir':
@@ -77,14 +133,15 @@ class Engine():
     #détermine les cases qu'une pièce peut jouer
     #retourne la nouvelle position et la piece à supprimer
     def cases_jouable(self, position_source, dic):
-        """Détermine les déplacements classiques possible.
+        """Détermine les déplacements classiques possiblent.
 
-        Attribut:
+        Args:
             position_source (dict): Clef et valeur d'une pièce.
+            dic (dict): Dictionnaire des positions.
 
-        return: 
-            list_dic (list): Une liste de dictionnaire représentant 
-            les positions obtenablent en déplacant la pièce en queston.
+        Returns: 
+            (list): Une liste de dictionnaire représentant les 
+            positions obtenablent en déplacant la pièce en queston.
         """
         #list des déplacement possible (dict)
         list_dic = []
@@ -109,11 +166,12 @@ class Engine():
     def cases_jouable_saut(self, position_source, dic):
         """Détermine les sauts possiblent.
 
-        Attribut:
+        Args:
             position_source (dict): Clef et valeur d'une pièce.
+            dic (dict): Dictionnaire des positions.
 
-        return: 
-            list_dic (list): Une liste de dictionnaire représentant 
+        Returns: 
+            (list): Une liste de dictionnaire représentant 
             les positions obtenablent en déplaçant la pièce en queston.
         """
         #list des déplacement possible (dict)
@@ -142,6 +200,16 @@ class Engine():
         return list_dic
 
     def dic_une_piece(self, position_source, dic):
+        """Détermine les déplacements et les sauts possiblent.
+
+        Args:
+            position_source (dict): Clef et valeur d'une pièce.
+            dic (dict): Dictionnaire des positions.
+
+        Returns:
+            (list), (bool): La liste des coups possiblent, True si les coups
+                sont des sauts et False s'ils sont des déplacements classiques.
+        """
         list_dic = []
         #ajout des sauts
         dic_saut = self.cases_jouable_saut(position_source, dic)
@@ -155,6 +223,15 @@ class Engine():
             return list_dic, False
     
     def iteration_dic_noir(self, dic):
+        """Détermine l'assemble des coups possibles pour les noirs.
+
+        Args:
+            dic (dict): Distionnaire des positions.
+
+        Returns:
+            (list), (int): Liste des coups possiblent pour le joueur noirs,
+                Point attribué à un saut.
+        """
         dic_noir = self.dic_noir(dic)
         list_dic_saut = []
         list_dic = []
@@ -176,6 +253,15 @@ class Engine():
             return list_dic, 0
 
     def iteration_dic_blanc(self, dic):
+        """Détermine l'assemble des coups possibles pour les blancs.
+
+        Args:
+            dic (dict): Distionnaire des positions.
+
+        Returns:
+            (list), (int): Liste des coups possiblent pour le joueur blanc,
+                Point attribué à un saut.
+        """
         dic_blanc = self.dic_blanc(dic)
         list_dic_saut = []
         list_dic = []
@@ -197,12 +283,24 @@ class Engine():
             return list_dic, 0
     
     def debutant_noir(self, dic):
+        """Détermine un coup aléatoire pour le joueur noir.
+        Args:
+            dic (dict): Dictionnaire de la position.
+        Returns:
+            (dict): Un coup aléatoire et légale.
+        """
         list_dic, saut = self.iteration_dic_noir(dic)
         numero_dic_choisit = randrange(len(list_dic))
         dic_choisit = list_dic[numero_dic_choisit]
         return dic_choisit
 
     def debutant_blanc(self, dic):
+        """Détermine un coup aléatoire pour le joueur blanc.
+        Args:
+            dic (dict): Dictionnaire de la position.
+        Returns:
+            (dict): Un coup aléatoire et légale.
+        """
         list_dic, saut = self.iteration_dic_blanc(dic)
         numero_dic_choisit = randrange(len(list_dic))
         dic_choisit = list_dic[numero_dic_choisit]
@@ -252,7 +350,7 @@ if __name__ == "__main__":
     for i in range(10):
         dic = Engine().avance(dic)
         Engine().print_damier(dic)
-        dic = Engine().avance_blanc(dic)
+        dic = Engine().debutant_blanc(dic)
         Engine().print_damier(dic)
 
 
--- test_commit.py
+++ test_commit.py
@@ -1,10 +1,2 @@
-def une_drole_de_recursion(liste):
-    n = len(liste)
-    if n == 0:
-            return True
-    
-    avant = une_drole_de_recursion(liste[0:-1])
-    if n < 3:
-        return avant and liste[n-1] == 1
-    else:
-        return avant and liste[n-1] == liste[n-2] + liste[n-3]
+x = '10', 6
+print(x[1])
