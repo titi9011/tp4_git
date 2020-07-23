36187e81cbde877a93fad1eb0cec83946c766e8f
titi9011 <thierry.blais.1@ulaval.ca>
2020-07-23 18:04:40 -0400

    ogn

--- interface_multi.py
+++ interface_multi.py
@@ -2,5 +2,12 @@ from engine import avance, avance_blanc
 from interface_dames import FenetrePartie
 
 class Interface_multi(FenetrePartie):
-    pass
+
+    def deux_joueurs(self):
+        super.__init__()
+    
+    def un_joueur(self):
+        super.__init__()
+
+        
 
