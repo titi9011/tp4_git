e20a685ba6cbe7572fd840cbb9b76cc0261b22c7
titi9011 <thierry.blais.1@ulaval.ca>
2020-07-23 18:12:15 -0400

    ouhewgoiuh

--- interface_multi.py
+++ interface_multi.py
@@ -1,13 +1,7 @@
-from engine import avance, avance_blanc
+from un_joueur import Un_joueur
 from interface_dames import FenetrePartie
 
-class Interface_multi(FenetrePartie):
 
-    def deux_joueurs(self):
-        super.__init__()
-    
-    def un_joueur(self):
-        super.__init__()
 
-        
+
 
---
+++ un_joueur.py
@@ -0,0 +1,7 @@
+from engine import *
+from interface_dames import FenetrePartie
+
+class Un_joueur(FenetrePartie):
+    def __init__(self):
+        super.__init__()
+        
