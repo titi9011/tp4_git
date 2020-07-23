8372c62e2fd0855f21a62f4b78e5e50d5fac6f42
bsevigny <bernard.sevigny.1@ulaval.ca>
2020-07-22 20:05:20 -0400

    Ça avance!!

--- interface_dames.py
+++ interface_dames.py
@@ -365,21 +365,30 @@ class FenetrePartie(Tk):
 
     def quitter_damier(self):
         self.fenetre_3 = Tk()
+        self.fenetre_3.geometry("500x400")
         self.fenetre_3.title("Pourquoi quitter?")
 
-        texte_3 = Message(self.fenetre_3)
-        texte_3['text'] = "Si vous ouvrez une nouvelle partie, une partie non terminée sera accessible et " \
-                          "il sera possible de jouer deux parties à la fois!\n En annulant, vous retournez à" \
-                          " la partie déjà ouverte."
-        texte_3.grid()
+        # texte_3_A = Message(self.fenetre_3)
+        texte_3_A = Label(self.fenetre_3)
+        texte_3_B = Label(self.fenetre_3)
+        texte_3_C = Label(self.fenetre_3)
+        texte_3_D = Label(self.fenetre_3)
+        texte_3_A['text'] = "Si vous ouvrez une nouvelle partie, la partie non terminée que vous venez de quitter"
+        texte_3_B['text'] = "sera encore accessible et il sera possible de jouer deux parties à la fois!\n "
+        texte_3_C['text'] = "En annulant, vous retournez à la partie déjà ouverte.\n "
+        texte_3_D['text'] = "Si vous quittez sans sauvegarder, toutes les parties seront fermées."
+        texte_3_A.grid(sticky=W)
+        texte_3_B.grid(sticky=W)
+        texte_3_C.grid()
+        texte_3_D.grid()
         bouton3_A = Button(self.fenetre_3, text='Quitter et sauvegarder', command=self.sauvegarde_partie)
         bouton3_B = Button(self.fenetre_3, text='Quitter sans sauvegarder', command=self.quit)
         bouton3_C = Button(self.fenetre_3, text='Nouvelle partie', command=self.nouvelle_partie)
         bouton3_D = Button(self.fenetre_3, text='Annuler', command=self.fenetre_quit_annulee)
-        bouton3_A.grid()
-        bouton3_B.grid()
-        bouton3_C.grid()
-        bouton3_D.grid()
+        bouton3_A.grid(row=4, column=0, pady=10, sticky=W)
+        bouton3_B.grid(row=4, column=1, pady=10, sticky=W)
+        bouton3_C.grid(row=5, column=0, sticky=W)
+        bouton3_D.grid(row=5, column=1)
         self.fenetre_3.tkraise()
         # Boutons à activer :
             # Quitter et sauvegarder
