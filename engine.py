1381274b433c3fbfe765c284176f8302f55105ac
bsevigny <bernard.sevigny.1@ulaval.ca>
2020-07-25 08:56:47 -0400

    Menu jouer contre ordi à compléter.

--- engine.py
+++ engine.py
@@ -1,11 +1,11 @@
 # Auteurs: Thierry Blais et Bernard Sévigny
 
-
 from partie import Partie
 from position import Position
 from piece import Piece
 from interface_dames import FenetrePartie
 from damier import Damier
+from canvas_damier import CanvasDamier
 from random import randrange
 from time import sleep
 from copy import deepcopy
@@ -363,6 +363,7 @@ def prise_obligatoire_noir(dic):
     return prise
 
 def avance(dic):
+    # CanvasDamier.actualiser()
     saut = True
     while saut:
         dic = avance_sans_prise(dic)
