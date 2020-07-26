5e065673bd29efdbeb7b43f1735255a9991300e5
titi9011 <thierry.blais.1@ulaval.ca>
2020-07-26 00:05:48 -0400

    ghlkj

--- interface_multi.py
+++ interface_multi.py
@@ -2,7 +2,6 @@ from un_joueur import Un_joueur
 from interface_dames import FenetrePartie
 from tkinter import *
 
-fenetre = Tk()
 
 fenetre = Tk()
 button = Button(fenetre, text='Un joueur', command=lambda:[fenetre.withdraw(), Un_joueur()], height = 10, width = 30)
