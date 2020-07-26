44542e47c98fce9ffea95e372a53d54febf34dd4
titi9011 <thierry.blais.1@ulaval.ca>
2020-07-25 23:57:10 -0400

    jg

--- interface_multi.py
+++ interface_multi.py
@@ -3,11 +3,10 @@ from interface_dames import FenetrePartie
 from tkinter import *
 
 
-def multi_joueurs():
-    fenetre = Tk()
-    button = Button(fenetre, text='Un joueur', command=lambda:[fenetre.withdraw(), Un_joueur()], height = 10, width = 30)
-    button.pack()
-    button2 = Button(fenetre, text='Deux joueurs', command=lambda:[fenetre.withdraw(), FenetrePartie()], height = 10, width = 30)
-    button2.pack()
+fenetre = Tk()
+button = Button(fenetre, text='Un joueur', command=lambda:[fenetre.withdraw(), Un_joueur()], height = 10, width = 30)
+button.pack()
+button2 = Button(fenetre, text='Deux joueurs', command=lambda:[fenetre.withdraw(), FenetrePartie()], height = 10, width = 30)
+button2.pack()
 
-    fenetre.mainloop()
+fenetre.mainloop()
