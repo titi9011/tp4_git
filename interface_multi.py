0a0989d590fd9d9cfc937759bdf0e5c7fffc6ee9
titi9011 <thierry.blais.1@ulaval.ca>
2020-07-25 22:40:53 -0400

    kjdkjd

--- interface_multi.py
+++ interface_multi.py
@@ -1,5 +1,9 @@
 from un_joueur import Un_joueur
 from interface_dames import FenetrePartie
+from tkinter import *
 
+fenetre = Tk()
 
-
+button = Button(fenetre, text='Un joueur', command=Un_joueur())
+button.pack()
+fenetre.mainloop()
